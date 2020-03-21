5b80b4204ebac303fbf60737d733f8a8c22853e4
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2020-03-21 10:32:45 -0400

    Added missing security settings

--- explorecali/src/main/resources/data.sql
+++ explorecali/src/main/resources/data.sql
@@ -418,3 +418,25 @@ insert into tour  (tour_package_code, title, description, blurb, bullets, diffic
   (1, 10, 1, 'I hated it'),
   (1, 11, 1, 'We hated it'),
   (2, 100, 5, 'I really thought it could have been better');
+  
+  
+-- USER
+-- non-encrypted password: letmein
+INSERT INTO security_user (id, username, password, first_name, last_name) VALUES
+(1,  'admin', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Administrator', 'Adminstrator'),
+(2,  'csr_jane', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Jane', 'Doe'),
+(3,  'csr_mark', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Mark', 'Smith'),
+(4,  'wally', '$2a$12$ZhGS.zcWt1gnZ9xRNp7inOvo5hIT0ngN7N.pN939cShxKvaQYHnnu', 'Walter', 'Adams');
+
+-- ROLES
+
+INSERT INTO security_role (id, role_name, description) VALUES (1, 'ROLE_ADMIN', 'Administrator');
+INSERT INTO security_role (id, role_name, description) VALUES (2, 'ROLE_CSR', 'Customer Service Representative');
+
+INSERT INTO user_role(user_id, role_id) VALUES
+ (1, 1), -- give admin ROLE_ADMIN
+ (2, 2),  -- give Jane ROLE_CSR
+ (3, 2),  -- give Mark ROLE_CSR
+ (4, 1),  -- give Wally ROLE_ADMIN
+ (4, 2);  -- give Wally ROLE_CSR
+ 
