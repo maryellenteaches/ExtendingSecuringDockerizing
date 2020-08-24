bc754ed68eb8bc645483ef352d8e6d00bf1362e1
miellyellen <maryellen.bowman@gmail.com>
2020-08-24 18:24:53 -0400

    correct test, remove swagger

--- explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
+++ explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -1,7 +1,5 @@
 package com.example.ec;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Mary Ellen Bowman
  */
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Explore California API",
-				description = "API Definitions of the Explore California Microservice",
-				version = "3.0.1"
-
-		))
 public class ExplorecaliApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExplorecaliApplication.class, args);
